8392ffafeb7fa28f19de073516856a38b3d81424
Annyv2 <Annyv2@users.noreply.github.com>
2016-02-05 15:10:33 -0430

    update example to work with redirect mode

--- examples/ember2-auth0-sample/app/routes/application.js
+++ examples/ember2-auth0-sample/app/routes/application.js
@@ -2,10 +2,19 @@ import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
 export default Ember.Route.extend(ApplicationRouteMixin, {
+  
+  //This example implements redirect mode https://auth0.com/docs/libraries/lock/authentication-modes
+  // You can delete the init function if you want to use popup mode
+  init:function(){
+      var lockOptions = {popup:false,clicked:false};
+      this.get('session').authenticate('simple-auth-authenticator:lock', lockOptions);
+  },
 
   actions: {
     login () {
-      var lockOptions = {authParams:{scope: 'openid'}};
+      //For popup mode. just put popup:true, you can delete the clicked option
+      //For redirect mode. popup:false, clicked:true
+      var lockOptions = {popup:false,clicked:true,authParams:{scope: 'openid'}};
       this.get('session').authenticate('simple-auth-authenticator:lock', lockOptions);
     },
 
